257f79c64645b044fb952d7c9116cca4db6df527
gegemonTV <vovakonki776@gmail.com>
2021-03-07 23:32:21 +0300

    updated activities

--- app/src/main/java/com/psychosocial/psychotestvizualizer/MainActivity.java
+++ app/src/main/java/com/psychosocial/psychotestvizualizer/MainActivity.java
@@ -3,12 +3,38 @@ package com.psychosocial.psychotestvizualizer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner ChooseSpinner;
+    String[] SpinnerElements = {"Социометрический тест", "IQ тест (coming soon)"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ChooseSpinner = (Spinner) findViewById(R.id.choose_type);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, SpinnerElements);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ChooseSpinner.setAdapter(adapter);
+        ChooseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
